88a995fe53cd9584a5f021ac41e7417ddb858842
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-07 16:36:06 -0400

    Add project files.

---
+++ DotnetWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotnetWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
