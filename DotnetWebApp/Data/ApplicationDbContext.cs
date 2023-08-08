ea0beee9fb91a3105995d2d010b944d8ace9d168
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-08 15:13:28 -0400

    Setup database

---
+++ DotnetWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DotnetWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
+
