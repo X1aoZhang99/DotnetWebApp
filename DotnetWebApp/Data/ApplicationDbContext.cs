8f568487a3c5e4ab6f98082ec62f548ec20dc875
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-09 19:11:54 -0400

    Add layout and setup serverside validation

--- DotnetWebApp/Data/ApplicationDbContext.cs
+++ DotnetWebApp/Data/ApplicationDbContext.cs
@@ -11,6 +11,22 @@ namespace DotnetWebApp.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Fiction", DisplayOrder = "1" },
+                new Category { Id = 2, Name = "Non-Fiction", DisplayOrder = "2" },
+                new Category { Id = 3, Name = "Sci-Fi", DisplayOrder = "3" },
+                new Category { Id = 4, Name = "Fantasy", DisplayOrder = "4" },
+                new Category { Id = 5, Name = "Mystery", DisplayOrder = "5" },
+                new Category { Id = 6, Name = "Thriller", DisplayOrder = "6" },
+                new Category { Id = 7, Name = "Romance", DisplayOrder = "7" },
+                new Category { Id = 8, Name = "Westerns", DisplayOrder = "8" },
+                new Category { Id = 9, Name = "Dystopian", DisplayOrder = "9" },
+                new Category { Id = 10, Name = "Contemporary", DisplayOrder = "10" }
+                );
+        }
     }
 }
 
--- DotnetWebApp/Models/Category.cs
+++ DotnetWebApp/Models/Category.cs
@@ -7,7 +7,11 @@ namespace DotnetWebApp.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [Display(Name="Category Name")]
+        [MaxLength(50)]
         public string Name { get; set; }
+        [Display(Name="Display Order")]
+        [Range(1,100, ErrorMessage="Display Order for category must be between 1-100")]
         public string DisplayOrder { get; set; }
     }
 }
