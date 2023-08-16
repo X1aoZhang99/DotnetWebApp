cf56f32863ce04f4ea6b3833abcdd62be01ea4f7
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-16 16:30:49 -0400

    Configure a razor webapp

---
+++ RazerWebApp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazerWebApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Category Name")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order for category must be between 1-100")]
+        public string DisplayOrder { get; set; }
+    }
+}
