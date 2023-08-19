68efc567987b957eada9282c68ad487031c9e68e
Xiao Zhang <zhang.xiao12@northeastern.edu>
2023-08-19 16:29:28 -0400

    Add image update

--- WebApp.DataAccess/Repository/ProductRepository.cs
+++ WebApp.DataAccess/Repository/ProductRepository.cs
@@ -20,7 +20,23 @@ namespace WebApp.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            var objFromDb = _db.Products.FirstOrDefault(s => s.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                if (obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+                objFromDb.Title = obj.Title;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Price = obj.Price;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.Description = obj.Description;
+                objFromDb.CategoryId = obj.CategoryId;
+                objFromDb.Author = obj.Author;
+            }
         }
     }
 }
--- WebApp.DataAccess/Repository/Repository.cs
+++ WebApp.DataAccess/Repository/Repository.cs
@@ -19,22 +19,37 @@ namespace WebApp.DataAccess.Repository
         {
             _db = db;
             this.dbSet = _db.Set<T>();
+            _db.Products.Include(u => u.Category);
         }
         public void Add(T entity)
         {
             dbSet.Add(entity);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter, string? incluedeProperty = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
+            if (!string.IsNullOrEmpty(incluedeProperty))
+            {
+                foreach (var includeProp in incluedeProperty.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? incluedeProperty = null)
         {
             IQueryable<T> query = dbSet;
+            if (!string.IsNullOrEmpty(incluedeProperty))
+            {
+                foreach(var includeProp in incluedeProperty.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.ToList();
         }
 
